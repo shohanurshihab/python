a682db069170151c3a969dd05a75b132178b146b
shohanurshihab <skshihab531@gmail.com>
2023-06-25 00:38:32 +0600

    n

--- guess_game.py
+++ guess_game.py
@@ -2,14 +2,14 @@ import random
 number = random.randint(1,10)
 print("Welcome to the game : Guess the number \n Your name : ",end="")
 name = input()
-print("Hey",name,", try to guess the number I have in my mind!")
+print("Hey",name,", try to guess the number I have in my mind! \n Your guess : ",end="")
 
 for guesstaken in range(1,4):
     guess = int(input())
     if guess < number:
-        print("Try a bit higher!")
+        print("Try a bit higher! -> ",end="")
     elif guess > number:
-        print("Try a bit lower!")
+        print("Try a bit lower! -> ",end="")
     else:
         break
 if number == guess:
